7aa4889093fc0f05ce7dcc078d465e90c1729225
Battypompano <107274337+Battypompano@users.noreply.github.com>
2022-06-10 22:45:48 -0600

    Add files via upload

---
+++ clock.py
@@ -0,0 +1,30 @@
+from tkinter import Tk
+from tkinter import Label
+import time
+import sys
+from datetime import date
+
+master = Tk()
+master.title("CLOCK")
+master.configure(bg="Black")
+
+def get_date():
+    datevar = date.today()
+    date1.config(text=datevar)
+    date1.after(200,get_date)
+
+def get_time():
+    timeVar = time.strftime("%I:%M:%S %p")
+    clock.config(text=timeVar)
+    clock.after(200,get_time)
+
+
+clock = Label(master, font=("Arial",100),bg="black",fg="white")
+clock.pack()
+date1 = Label(master, font=("Arial",50),fg="white",bg="black")
+date1.pack()
+
+get_date()
+get_time()
+
+master.mainloop()
